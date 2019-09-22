249fb1f7e0f443166620238283398f73c2c000e2
Frederik Glücks <frederik@gluecks-online.de>
2019-09-22 22:46:37 +0200

    Create logs dir if not exists

--- libs/Logger.php
+++ libs/Logger.php
@@ -11,6 +11,10 @@ class Logger
 
 	public static function write($text)
 	{
+		if (!file_exists(self::$logDir)) {
+			mkdir(self::$logDir);
+		}
+
 		$text = date('Y-m-d H:i:s O') . " | " . $text . "\n";
 
 		if (!file_put_contents(self::$logDir . self::$logFile, $text, FILE_APPEND)) {
